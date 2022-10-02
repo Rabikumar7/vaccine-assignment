0019f731ecb73d28211b0057bae8205e6ea05c2e
Rabi rabikumar <rabindrakumarmuli@gmail.com>
2022-10-03 00:22:26 +0530

    userScript-DOM

--- scripts/userScript.js
+++ scripts/userScript.js
@@ -2,6 +2,7 @@ let formdata=JSON.parse(localStorage.getItem("localdata"))||[]
     let form=document.getElementById("form")
     form.addEventListener("submit",function(){
         // event.preventDefault()
+        
         let name=document.getElementById("name").value
         let email=document.getElementById("email").value
         let age=document.getElementById("age").value
